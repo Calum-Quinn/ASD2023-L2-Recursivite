7d9cb389ab953c01fd1474b7e13fe99de8747b18
calum <calum.quinn@heig-vd.ch>
2023-03-09 13:04:52 +0100

    Changes by dariovasq

--- src/controle.cpp
+++ src/controle.cpp
@@ -1,3 +1,13 @@
+/* -----------------------------------------------------------------------------------
+Nom du fichier : controle.cpp (xxx = h ou cpp)
+Auteur(s)      : Ewan Mariaux, Calum Quinn, Dario Vasques
+Date creation  : 09.03.2023
+Description    : Programme principal
+Remarque(s)    : <à compléter>
+Compilateurs   : Apple clang version 14.0.0 (clang-1400.0.29.102) (Dario)
+               : Mingw-w64 g++ 12.2.0 (Calum et Ewan)
+----------------------------------------------------------------------------------- */
+
 #include "controle.h"
 
 using namespace std;
