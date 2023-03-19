ccc433e032ec0e33809cc672a38982260eec674c
dariovasq <dario.vasques@heig-vd.ch>
2023-03-19 17:02:03 +0100

    Algo final

--- src/Carte.cpp
+++ src/Carte.cpp
@@ -8,51 +8,43 @@ Compilateurs   : Apple clang version 14.0.0 (clang-1400.0.29.102) (Dario)
                : Mingw-w64 g++ 12.2.0 (Calum et Ewan)
 ----------------------------------------------------------------------------------- */
 
-#include "Carte.h"
-
 #include <algorithm>
+
+#include "Carte.h"
 
 using namespace std;
 
-Carte::Carte(const int id, Piece motifs) {
-    this->id = id;
-    this->rotation = 'a';
-    this->rot = 0;
-    this->motifs = motifs;
-//    this->position = 9;
-    this->position = id - 1;
+Carte::Carte(const unsigned id, Piece motifs) {
+   this->id = id;
+   this->rot = 0;
+   this->motifs = motifs;
+   this->position = id - 1;
 }
 
 Carte::Carte(const Carte& autre) : position(autre.position){
-    *this = autre;
+   *this = autre;
 }
 
 void Carte::tourner(){
-    //On augmente une valeur represantant la rotation de sorte à pouvoir réinitialiser le plateau
-    rot = ++rot % 4;
-}
-
-void Carte::reset() {
-    rot = 0;
-    position = id - 1;
+   //On augmente une valeur represantant la rotation de sorte à pouvoir réinitialiser le plateau
+   rot = ++rot % 4;
 }
 
 Carte& Carte::operator= (const Carte& autre) {
-    this->motifs = autre.motifs;
-    this->rotation = autre.rotation;
-    this->rot = autre.rot;
-    this->id = autre.id;
+   this->motifs = autre.motifs;
+   this->rot = autre.rot;
+   this->id = autre.id;
 
-    return *this;
+   return *this;
 }
 
 std::ostream& operator<<(std::ostream& os, const Carte& carte){
 
-    os << "id " << carte.getId() << " : " ;
-    for (size_t i = 0; i < carte.motifs.size(); ++i) {
-        os << carte.motifs[i] << " ";
-    }
-    os << "rotation :" << carte.rot << " position :" << carte.position << endl;
+   os << "id " << carte.getId() << " : " ;
+   for (int motif : carte.motifs) {
+      os << motif << " ";
+   }
+   os << "rotation :" << carte.rot << " position :" << carte.position << endl;
 
-    return os;
+   return os;
 }
--- src/Carte.h
+++ src/Carte.h
@@ -11,50 +11,127 @@ Compilateurs   : Apple clang version 14.0.0 (clang-1400.0.29.102) (Dario)
 #ifndef ASD2022_L2_RECURSIVITE_CARTE_H
 #define ASD2022_L2_RECURSIVITE_CARTE_H
 
-#include "pieces.h"
-
 #include <iostream>
 #include <array>
 #include <vector>
 
+#include "pieces.h"
+
 class Carte {
 public:
-    Carte() = default;
-    Carte(const int id, Piece motifs);
-    Carte(const Carte& c);
+   Carte() = default;
 
-    friend std::ostream& operator<<(std::ostream& os, const Carte& carte);
+   /**
+    * @name                : Carte
+    *
+    * @but                 : Constructeur avec paramètre
+    *
+    * @param id            : identificateur unique
+    * @motifs              : vecteur contenant les images sur la carte
+    * @return              : void
+    * @throws              : NIL
+    */
+   Carte(const unsigned id, Piece motifs);
 
-    friend bool controle(const std::vector<Carte>& cartes, Carte& carte);
+   /**
+    * @name                : Carte
+    *
+    * @but                 : Constructeur par copie
+    *
+    * @param autre         : Carte à copier
+    * @return              : void
+    * @throws              : NIL
+    */
+   Carte(const Carte& c);
 
-    Carte& operator= (const Carte& autre);
-    void tourner();
-    void reset();
+   /**
+    * @name            : tourner
+    *
+    * @but             : Fonction qui tourne une carte
+    *
+    * @return          : void
+    * @throws          : NIL
+    */
+   void tourner();
 
+   /**
+    * @name            : operator=
+    *
+    * @but             : Opérateur d'affectation
+    *
+    * @param autre     : Carte à affecter
+    * @return          : La carte
+    * @throws          : NIL
+    */
+   Carte& operator= (const Carte& autre);
 
-    unsigned getId() const{
-        return id;
-    }
-    size_t getPosition() const{
-        return position;
-    }
-    char getRotation() const{
-        return rotation;
-    }
-    int getRot() const {
-        return rot;
-    }
-    Piece getMotifs() const{
-        return motifs;
-    }
+   /**
+    * @name            : operator<<
+    *
+    * @but             : Afficher une carte
+    *
+    * @param os        : Flux de sortie
+    * @param carte     : Carte à afficher
+    * @return          : Le flux de sortie
+    * @throws          : NIL
+    */
+   friend std::ostream& operator<<(std::ostream& os, const Carte& carte);
+
+   /**
+    * @name            : getId
+    *
+    * @but             : Fonction qui retourne l'identificateur d'une carte
+    *
+    * @return          : L'identificateur
+    * @throws          : NIL
+    */
+   unsigned getId() const{
+      return id;
+   }
+
+   /**
+    * @name            : setPosition
+    *
+    * @but             : Fonction qui permet de donner une position à une carte
+    *
+    * @param pos       : Position à donner à la carte
+    * @return          : void
+    * @throws          : NIL
+    */
+   void setPosition(size_t pos) {
+      position = pos;
+   }
+
+   /**
+    * @name            : getRot
+    *
+    * @but             : Fonction qui retourne la rotation d'une carte
+    *
+    * @return          : La rotation
+    * @throws          : NIL
+    */
+   int getRot() const {
+      return rot;
+   }
+
+   /**
+    * @name            : getMotifs
+    *
+    * @but             : Fonction qui retourne le vecteur des motifs d'une carte
+    *
+    * @return          : Le vecteur de motifs
+    * @throws          : NIL
+    */
+   Piece getMotifs() const{
+      return motifs;
+   }
 
 
 private:
-    unsigned id;
-    size_t position;
-    char rotation;
-    int rot;
-    Piece motifs;
+   unsigned id;
+   size_t position;
+   int rot;
+   Piece motifs;
 };
 
 
