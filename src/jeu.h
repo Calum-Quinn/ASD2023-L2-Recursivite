3eeb59269b79c81086454ed9827de524eba1cf52
calum <calum.quinn@heig-vd.ch>
2023-03-19 16:45:26 +0100

    Changes by dariovasq

--- src/jeu.h
+++ src/jeu.h
@@ -2,7 +2,7 @@
 Nom du fichier : jeu.h (xxx = h ou cpp)
 Auteur(s)      : Ewan Mariaux, Calum Quinn, Dario Vasques
 Date creation  : 09.03.2023
-Description    : Programme lançant l'algorithme du jeu.
+Description    : Programme lançant l'algorithme du jeu afin de trouver toutes les solutions possibles.
 Remarque(s)    : <à compléter>
 Compilateurs   : Apple clang version 14.0.0 (clang-1400.0.29.102) (Dario)
                : Mingw-w64 g++ 12.2.0 (Calum et Ewan)
